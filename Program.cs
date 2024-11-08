e497a1d54687b5ff2e6e9ce407d6145d3952649c
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-08 07:02:38 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//*****************************************************************************
+//** 1829. Maximum XOR for Each Query    leetcode                            **
+//*****************************************************************************
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getMaximumXor(int* nums, int numsSize, int maximumBit, int* returnSize) {
+    *returnSize = numsSize;
+    int* ans = (int*)malloc(numsSize * sizeof(int));
+
+    int xs = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        xs ^= nums[i];
+    }
+
+    int mask = (1 << maximumBit) - 1;
+
+    for (int i = 0; i < numsSize; ++i) {
+        int x = nums[numsSize - i - 1];
+        ans[i] = xs ^ mask;
+        xs ^= x;
+    }
+
+    return ans;
+}
